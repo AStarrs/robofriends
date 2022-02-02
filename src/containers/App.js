96200da80069f740c89bbf92ddf374547fae9a32
Anna S <ann.starovoytova@gmail.com>
2022-02-03 09:51:27 +1300

    converted class component App into stateful functional component using React hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -6,27 +6,34 @@ import ErrorBoundary from "../components/ErrorBoundary";
 import './App.css';
 
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+function App() {
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: ''
+    //     }
+    // }
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
 
-    componentDidMount() {
+    // componentDidMount() {
+    //     fetch ('https://jsonplaceholder.typicode.com/users')
+    //         .then(response=> response.json())
+    //         .then(users=> this.setState({ robots: users}));
+    // }
+    useEffect(()=> {
         fetch ('https://jsonplaceholder.typicode.com/users')
             .then(response=> response.json())
-            .then(users=> this.setState({ robots: users}));
+            .then(users=> {setRobots(users)});
+    }, [])
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield:event.target.value });
-    }
-
-    render() {
-        const { robots, searchfield } = this.state
+    // render() {
+        // const { robots, searchfield } = this.state
         const filteredRobots = robots.filter(robots => {
             return robots.name.toLowerCase().includes(searchfield.toLowerCase());
         });
@@ -35,7 +42,7 @@ class App extends Component {
                 (
                     <div className='tc'>
                         <h1 className='f1'>RoboFriends</h1>
-                        <SearchBox searchChange={this.onSearchChange}/>
+                        <SearchBox searchChange={onSearchChange}/>
                         <Scroll>
                             <ErrorBoundary>
                                 <CardList robots={filteredRobots} />
@@ -43,7 +50,7 @@ class App extends Component {
                         </Scroll>
                     </div>
                 );
-     }
+    //  }
 }
 
-export default App
+export default App;
